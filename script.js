95fda1a60f9fb78d1df59fbaebdaa486ffd60bd5
LÍVIA DE SOUZA CAETANO <livia.caetano4@senaisp.edu.br>
2026-03-13 10:27:03 -0300

    ex6

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-
 const inputTarefa = document.getElementById("inputTarefa");
 const btnAdicionar = document.getElementById("btnAdicionar");
 const listaTarefas = document.getElementById("listaTarefas");
 const mensagem = document.getElementById("mensagem");
 
 btnAdicionar.addEventListener("click", function () {
-let texto = inputTarefa.value;
+    let texto = inputTarefa.value.trim();
+
     if (texto === "") {
         mensagem.textContent = "Ei, escreve alguma coisa aí!";
-        mensagem.className = "text-danger fw-bold";
-    } else {
-
-    const li = document.createElement("li");
-    li.className = "list-group-item";
-    li.textContent = texto;
-    listaTarefas.appendChild(li);
-    inputTarefa.value = "";
-
- mensagem.textContent = "Eba, tarefa adicionada com sucesso!";
- mensagem.className = "text-success fw-bold";
+        mensagem.className = "p-2 bg-danger text-white rounded fw-bold";
+        return;
     }
 
+  
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+
+    const divConteudo = document.createElement("div");
+    divConteudo.className = "form-check d-flex align-items-center";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "form-check-input me-2";
+
+
+    divConteudo.appendChild(checkbox);
+    divConteudo.appendChild(document.createTextNode(texto));
+
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            divConteudo.style.textDecoration = "line-through";
+        } else {
+            divConteudo.style.textDecoration = "none";
+            divConteudo.style.color = "black";
+        }
+    });
+
+    const btnRemover = document.createElement("button");
+    btnRemover.textContent = "Remover";
+    btnRemover.className = "btn btn-danger btn-sm";
+
+    btnRemover.addEventListener("click", function () {
+        li.remove();
+        mensagem.textContent = "Tarefa removida!";
+        mensagem.className = "p-2 bg-warning text-dark rounded fw-bold";
+    });
+
+    li.appendChild(divConteudo);
+    li.appendChild(btnRemover);
+
+    listaTarefas.appendChild(li);
+
+    inputTarefa.value = "";
+
+    mensagem.textContent = "misericórdia, quanta coisa!";
+    mensagem.className = "p-2 bg-success text-white rounded fw-bold";
 });
