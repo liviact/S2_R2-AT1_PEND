410941e57b8ef0cceb1330e1d5863c729e86d12d
LÍVIA DE SOUZA CAETANO <livia.caetano4@senaisp.edu.br>
2026-03-13 09:31:38 -0300

    ex2 separado em pastas

---
+++ script.js
@@ -0,0 +1,9 @@
+function alterarTexto() {
+
+    const textoElemento = document.getElementById("meuTexto");
+
+    textoElemento.innerText = "✨ Mágica! O JS agiu aqui.";
+
+    textoElemento.style.color = "#871F78";
+
+}
